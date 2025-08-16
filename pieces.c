9457aca1626060586742c5bc1cb545a975eff6f8
eskezje <114604325+eskezje@users.noreply.github.com>
2025-08-16 14:58:34 +0200

    Update pieces.c

--- pieces.c
+++ pieces.c
@@ -46,12 +46,8 @@ int move_knight(struct Chess_move player_move) {
                 return 0;
             }
             
-            board[from_square] = EMPTY;
-            board[to_square] = piece;
+            move_actual_piece(player_move);
             found_move += 1;
-            printf("The piece has been moved from %c%c to %c%c\n", 
-                'a' + from_file, '1' + from_rank, 
-                'a' + to_file, '1' + to_rank);
             advance_round();
             break;
         } 
@@ -92,14 +88,66 @@ int move_pawn(struct Chess_move player_move) {
         move_len = 2;
     }
 
-    distance =abs(from_rank - to_rank);
+    distance = from_rank - to_rank;
     horizontal = from_file - to_file;
 
-    if (distance == move_len && 
-        move_len == 2 && 
-        horizontal == 0 && !(to_square & 0x88) && 
-        board[to_square] == EMPTY)   {
-            board[from_square] = EMPTY;
-            board[to_square]   = piece;
+    //black pawn
+    if (current_player < 0)  {
+        if (distance < 0 &&                 // they are always moving downwards on the board
+            horizontal == 0 &&              // they are not allowed to move horizontally (yet)
+            abs(distance) <= move_len &&    // they cant move further than what their move length is
+            board[to_square] == EMPTY)   {  // the square they are moving to needs to be empty, if moving only vertically
+                move_actual_piece(player_move);
+                found_move += 1;
+                advance_round();
+        }
     }
+
+    //black pawn
+    if (current_player > 0)  {
+        if (distance > 0 &&                 // they are always moving downwards on the board
+            horizontal == 0 &&              // they are not allowed to move horizontally (yet)
+            abs(distance) <= move_len &&    // they cant move further than what their move length is
+            board[to_square] == EMPTY)   {  // the square they are moving to needs to be empty, if moving only vertically
+                move_actual_piece(player_move);
+                found_move += 1;
+                advance_round();
+        }
+    }
+
+    if (!found_move) {
+        printf("The move was not legal, try again\n");
+    }
+
+    return found_move ? 1 : 0;
+}
+
+void print_has_move(struct Chess_move player_move)  {
+    int from_file = player_move.from.file;
+    int from_rank = player_move.from.rank;
+    
+    int to_file = player_move.to.file;
+    int to_rank = player_move.to.rank;
+    printf("The piece has been moved from %c%c to %c%c\n", 
+        'a' + from_file, '1' + from_rank, 
+        'a' + to_file, '1' + to_rank);
+}
+
+void move_actual_piece(struct Chess_move player_move)   {
+    // extract source position
+    int from_file = player_move.from.file;
+    int from_rank = player_move.from.rank;
+    int from_square = square_index(from_rank, from_file);
+    
+    // extract destination position
+    int to_file = player_move.to.file;
+    int to_rank = player_move.to.rank;
+    int to_square = square_index(to_rank, to_file);
+    
+    // get the piece at the source square
+    int piece = board[from_square];
+
+    board[to_square] = piece;
+    board[from_square] = EMPTY;
+    print_has_move(player_move);
 }
